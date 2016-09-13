f95c5423749c59cbb054b6b3e18ae9007c80da48
Papventje <robbierobsnor@gmail.com>
2016-09-13 15:48:45 +0200

    Camera done en andere stuff

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private CharacterController controller;
+    private Vector3 moveVector;
+
+    private float speed = 5f;
+    private float verticalVelocity = 0f;
+    private float gravity = 12f;
+
+    private float animationDuration = 3f;
+
+
+    // Use this for initialization
+    void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Time.time < animationDuration)
+        {
+            controller.Move(Vector3.forward * speed * Time.deltaTime);
+            return;
+        }
+
+        moveVector = Vector3.zero;
+
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -0.5f;
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+
+        moveVector.y = verticalVelocity;
+
+        moveVector.z = speed;
+        controller.Move(moveVector * Time.deltaTime);
+	}
+}
